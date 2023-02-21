cdc6b6733de8252841e136b1df723dfd6fe4d1a8
Harmanpreet Kaur <89505084+harman0108k@users.noreply.github.com>
2023-02-21 20:31:12 +0530

    Add project files.

---
+++ fluent-aspnet/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace fluent_aspnet.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
